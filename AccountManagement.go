a4bc214f4eb1995e7f5943f211eaa650e0fbb87e
Vlad Oblamskyi <vlad.oblamskyi@intellecteu.com>
2017-01-31 05:13:15 -0500

    AccountManagement

--- AccountManagement.go
+++ AccountManagement.go
@@ -168,7 +168,7 @@ func (t *AccountManagement) Query(stub shim.ChaincodeStubInterface, function str
 			}
 			if account != nil {
 				accountView := &AccountView {
-					Id: b64.StdEncoding.EncodeToString(jsonUserKey),
+					Id: b64.StdEncoding.EncodeToString(jsonAccountKey),
 					BIC: userDetails.Permissions[i].Key.Owner,
 					Number: accountValue.Number,
 					Amount: accountValue.Amount,
